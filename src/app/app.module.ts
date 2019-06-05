fb34f34cbea1d46f0627c0a7ba6ccaef3aaba64f
Rubén Garzón <ruben.garzon@atmira.com>
2019-06-05 13:57:57 +0200

    proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './views/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SliderComponent } from './components/slider/slider.component';
 import { AboutComponent } from './views/about/about.component';
 import { ProyectosComponent } from './views/proyectos/proyectos.component';
 import { TitleIconComponent } from './components/title-icon/title-icon.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { GaleryComponent } from './components/galery/galery.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { CardComponent } from './components/card/card.component';
 import { HabilidadesComponent } from './views/habilidades/habilidades.component';
 import { ProgressComponent } from './components/progress/progress.component';
@@ -26,13 +23,10 @@ import { CurriculumComponent } from './views/curriculum/curriculum.component';
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    SliderComponent,
     AboutComponent,
     ProyectosComponent,
     TitleIconComponent,
-    BreadcrumbComponent,
     GaleryComponent,
-    WelcomeComponent,
     CardComponent,
     HabilidadesComponent,
     ProgressComponent,
